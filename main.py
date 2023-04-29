af492ec0dedd5baecb6935498f71c43c2ba5b6e3
Vladimir Katin <51898657+dzailz@users.noreply.github.com>
2023-04-29 11:11:56 +0300

    Refactoring (#10)
    * add .gitignore
    
    * update versions in requirements.txt
    
    * tests refactoring
    
    * minor

--- main.py
+++ main.py
@@ -1,47 +1,42 @@
-#import io
 import streamlit as st
-#import torchaudio
 from speechbrain.pretrained import EncoderClassifier
 
-
 APP_NAME = 'Определение языка аудиофайла'
-APP_DESCRIPTION = '<i>Используемая модель: <a href="https://huggingface.co/speechbrain/lang-id-voxlingua107-ecapa/tree/main" target="_blank">Spoken Language Identification Model</a></i>'
+APP_DESCRIPTION = '<i>Используемая модель: ' \
+                  '<a href="https://huggingface.co/speechbrain/lang-id-voxlingua107-ecapa/tree/main" ' \
+                  'target="_blank">Spoken Language Identification Model</a></i>'
 APP_REP = '<i> <a href="https://github.com/Dk-A-r/SoundML" target="_blank">Репозиторий проекта</a></i>'
 
 
 def load_audio():
     """Создание формы для загрузки аудио"""
-
-    #Форма для загрузки средствами Streamlit
-
-    uploaded_file = st.file_uploader(
-        label='Загрузите аудиофайл для распознавания')
-
-    if uploaded_file is not None:
-        #Получение загруженного аудио
+    uploaded_file = st.file_uploader(label='Загрузите аудиофайл для распознавания')
+    if uploaded_file:
+        # Получение загруженного аудио
         audio_data = uploaded_file.getvalue()
-        #st.write(audio_data)
-        #Вывод аудиоплеера
+        # Вывод аудиоплеера
         st.audio(audio_data)
-        with open("temp.wav","wb") as f:
+        with open("temp.wav", "wb") as f:
             f.write(uploaded_file.getbuffer())
+
 
 @st.cache(allow_output_mutation=True)
 def model_loading():
-    return EncoderClassifier.from_hparams(source="speechbrain/lang-id-voxlingua107-ecapa", savedir="tmp")    
+    return EncoderClassifier.from_hparams(source="speechbrain/lang-id-voxlingua107-ecapa", savedir="tmp")
 
 
 def identify(language_id):
     signal = language_id.load_audio("temp.wav")
-    prediction =  language_id.classify_batch(signal)
+    prediction = language_id.classify_batch(signal)
     st.write(prediction[3][0] + ' with probability' + f" {prediction[1].exp().item()}")
+
 
 def main():
     st.set_page_config(page_title=APP_NAME)
     st.title(APP_NAME)
     st.markdown(APP_DESCRIPTION, True)
     st.markdown(APP_REP, True)
-    
+
     st.info('''Состав команды:  
         - Карпов Данил  
         - Репенко Диана''')
@@ -57,5 +52,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-    
--- test_sound.py
+++ test_sound.py
@@ -1,52 +1,19 @@
-import requests
-import streamlit as st
-from speechbrain.pretrained import EncoderClassifier
+import pytest
+from main import model_loading
 
 
-def load_audio():
-    """Создание формы для загрузки аудио"""
-
-    # Форма для загрузки средствами Streamlit
-    uploaded_file = st.file_uploader(
-        label='Загрузите аудиофайл для распознавания')
-
-    if uploaded_file is not None:
-        # Получение загруженного аудио
-        audio_data = uploaded_file.getvalue()
-
-        # Вывод аудиоплеера
-        st.audio(audio_data)
-        with open("temp.wav", "wb") as f:
-            f.write(uploaded_file.getbuffer())
+@pytest.fixture
+def language_id():
+    return model_loading()
 
 
-@st.cache(allow_output_mutation=True)
-def model_loading():
-    return EncoderClassifier.from_hparams(source="speechbrain/lang-id-voxlingua107-ecapa", savedir="tmp")
-
-
-def identify():
-    signal = language_id.load_audio("temp.wav")
-    prediction = language_id.classify_batch(signal)
-    st.write(prediction[3][0] + ' with probability' + f" {prediction[1].exp().item()}")
-
-
-load_audio()
-# Если кнопка нажата, то запускаем распознавание
-result = st.button('Распознать аудиофайл')
-
-if result:
-    language_id = model_loading()
-    identify()
-
-
-def test_load():
-    response = requests.get("https://huggingface.co/speechbrain/lang-id-voxlingua107-ecapa")
-    language_id = model_loading()
+def test_predict_russian(language_id):
     signal = language_id.load_audio("shortstories_childrenadults_0008.wav")
     prediction = language_id.classify_batch(signal)
     assert prediction[3][0] == 'ru: Russian'
+
+
+def test_predict_english(language_id):
     signal = language_id.load_audio("test2.wav")
     prediction = language_id.classify_batch(signal)
     assert prediction[3][0] == 'en: English'
-    assert response.status_code == 200
